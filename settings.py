59337c18f5299c3801cab65b2d4585f93c869764
xlbrycbx <qinpangji@gmail.com>
2022-05-13 17:17:51 +0800

    5-13pm

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/sqlite/sqlite-tools-win32-x86-3380500/try.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///D:/sqlite/sqlite-tools-win32-x86-3380500/try.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
